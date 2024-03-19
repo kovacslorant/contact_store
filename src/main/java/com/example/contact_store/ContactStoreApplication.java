c55dece5ccd0bab72dbe57fda3f78d3c1ee5e850
Kovacs Lorant <kovacslorant03@gmail.com>
2024-03-19 15:01:27 +0100

    init

---
+++ src/main/java/com/example/contact_store/ContactStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.contact_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactStoreApplication.class, args);
+    }
+
+}
