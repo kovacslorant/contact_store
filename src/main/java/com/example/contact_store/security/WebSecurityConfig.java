483f0cfcfd416496cd1e1bacf12e4d13aaf0d0e0
Kovacs Lorant <kovacslorant03@gmail.com>
2024-03-19 16:55:24 +0100

    Config SecurityFilterChain

---
+++ src/main/java/com/example/contact_store/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.contact_store.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher("/**")
+                .sessionManagement(sessionManagementConfigurer
+                        -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+
+        return http.build();
+    }
+}
